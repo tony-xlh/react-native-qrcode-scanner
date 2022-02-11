a17cf65e5b296e34267c6f38d40acf58e615fa95
xulihang <xulihanghai@163.com>
2022-02-11 17:40:42 +0800

    show barcode overlays using react-native-svg

--- App.tsx
+++ App.tsx
@@ -1,22 +1,52 @@
 import * as React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text } from 'react-native';
 import { Camera, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
 import { DBRConfig, decode, TextResult } from 'vision-camera-dynamsoft-barcode-reader';
 import * as REA from 'react-native-reanimated';
+import { Polygon, Rect, Svg } from 'react-native-svg';
 
 export default function App() {
   const [hasPermission, setHasPermission] = React.useState(false);
   const [barcodeResults, setBarcodeResults] = React.useState([] as TextResult[]);
+  const [frameWidth, setFrameWidth] = React.useState(720);
+  const [frameHeight, setFrameHeight] = React.useState(1280);
   const devices = useCameraDevices();
   const device = devices.back;
+  
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
     const config:DBRConfig = {};
-    config.template="{\"ImageParameter\":{\"BarcodeFormatIds\":[\"BF_QR_CODE\"],\"Description\":\"\",\"Name\":\"Settings\"},\"Version\":\"3.0\"}";
-
+    //config.template="{\"ImageParameter\":{\"BarcodeFormatIds\":[\"BF_QR_CODE\"],\"Description\":\"\",\"Name\":\"Settings\"},\"Version\":\"3.0\"}";
+    
     const results:TextResult[] = decode(frame,config)
+    
+    console.log("height: "+frame.height);
+    console.log("width: "+frame.width);
+    
     REA.runOnJS(setBarcodeResults)(results);
+    REA.runOnJS(setFrameWidth)(frame.width);
+    REA.runOnJS(setFrameHeight)(frame.height);
   }, [])
+
+  function getPointsData(lr:TextResult){
+    var pointsData = lr.x1 + "," + lr.y1 + " ";
+    pointsData = pointsData+lr.x2 + "," + lr.y2 +" ";
+    pointsData = pointsData+lr.x3 + "," + lr.y3 +" ";
+    pointsData = pointsData+lr.x4 + "," + lr.y4;
+    return pointsData;
+  }
+
+  function getViewBox(){
+    let viewBox = null;
+    if (frameHeight>frameWidth && Dimensions.get('window').width>Dimensions.get('window').height){
+      console.log("Has rotation");
+      viewBox = "0 0 "+frameHeight+" "+frameWidth;
+    }else {
+      viewBox = "0 0 "+frameWidth+" "+frameHeight;
+    }    
+    console.log("viewBox"+viewBox);
+    return viewBox;
+  }
 
   React.useEffect(() => {
     (async () => {
@@ -38,25 +68,33 @@ export default function App() {
             frameProcessorFps={5}
             />
             {barcodeResults.map((barcode, idx) => (
-            <Text key={idx} style={styles.barcodeTextURL}>
+            <Text key={idx} style={styles.barcodeText}>
                 {barcode.barcodeFormat +": "+ barcode.barcodeText}
             </Text>
             ))}
         </>)}
+        <Svg style={[StyleSheet.absoluteFill]} viewBox={getViewBox()}>
+
+          {barcodeResults.map((barcode, idx) => (
+            <Polygon key={idx}
+            points={getPointsData(barcode)}
+            fill="lime"
+            stroke="green"
+            opacity="0.5"
+            strokeWidth="1"
+          />
+          ))}
+          
+        </Svg>
       </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex:1
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-  barcodeTextURL: {
+  barcodeText: {
     fontSize: 20,
     color: 'white',
     fontWeight: 'bold',
